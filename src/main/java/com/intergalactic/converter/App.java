126aa5897c8d24aa62f4c5737123f8520a071571
lufuno maphwanya <lcmaphwanya@gmail.com>
2017-11-16 17:21:49 +0200

    adds filereader.

--- src/main/java/com/intergalactic/converter/App.java
+++ src/main/java/com/intergalactic/converter/App.java
@@ -1,12 +1,28 @@
 package src.main.java.com.intergalactic.converter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import static src.main.java.com.intergalactic.converter.NumberConversionUtil.romanToDecimal;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
+        String filename = "instructions.txt";
 
-        System.out.println(romanToDecimal("MCMXLIV"));
+        File inputFile = new File(filename);
+        BufferedReader buffer = new BufferedReader(new FileReader(inputFile));
+
+        String readLine = "";
+
+        while ((readLine = buffer.readLine()) != null) {
+            System.out.println(readLine);
+        }
+
+
+
+
     }
 }
